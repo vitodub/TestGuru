4b39efea09ca13402b66eb6fc511592d4982862d
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2022-10-02 22:22:56 +0500

    feat: association set up

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :usertests
+  belongs_to :category
+  belongs_to :author, class_name: :User, foreign_key: :author_id
+
 	def self.title_by_category(name)
 		Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
 		.where(categories: { title: name }).order(title: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-	def test_by_level(test_level)
-		Test.where(level: test_level)
-        .joins('INNER JOIN usertests ON tests.id = usertests.test_id')
-        .where(usertests: { user_id: self.id })
-    end
+  has_many :tests_users
+  has_many :tests, through: :usertests
+  has_many :authortests, class_name: :Test, foreign_key: :author_id
+
+
+  def test_by_level(test_level)
+    Test.where(level: test_level)
+    .joins('INNER JOIN usertests ON tests.id = usertests.test_id')
+    .where(usertests: { user_id: self.id })
+  end
 end
