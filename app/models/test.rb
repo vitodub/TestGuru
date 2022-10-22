43a18758c7ef86b5b3776eff72e198dee4df24c6
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2022-10-22 11:14:19 +0500

    refactor: model methods replaced with scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,13 +6,12 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: :User, foreign_key: :author_id
 
-  scope :select_easy, -> { where(level: 0..1) }
-  scope :select_medium, -> { where(level: 2..4) }
-  scope :select_hard, -> { where(level: 5..Float::INFINITY) }
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, ->(name) { joins(:category).where(category: { title: name }) }
 
   def self.title_by_category(name)
-    Test.joins(:category)
-    .where(category: { title: name }).order(title: :desc)
-    .pluck(:title)
+    by_category(name).order(title: :desc).pluck(:title)
   end
 end
