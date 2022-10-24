84af54b8ab753edfa132f2d809e5409406d7d311
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2022-10-24 11:29:00 +0500

    fix: style, associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,7 @@
 class Category < ApplicationRecord
-	has_many :tests, dependent: :destroy
+  default_scope { order(title: :asc) }
 
-	validates :title, presence: true
+  has_many :tests, dependent: :destroy
 
-	default_scope { order(title: :asc) }
-
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :usertests, dependent: :destroy
+  has_many :usertests
   has_many :tests, through: :usertests, dependent: :destroy
   has_many :authortests, class_name: :Test, foreign_key: :author_id, dependent: :destroy
 
