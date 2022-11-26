95ebbd965d1d3b3896e13601fd0e71a5ef36b169
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2022-11-26 22:57:40 +0500

    feat: files for answers added

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validates :less_than_4_answers, on: :create
+  validate :less_than_4_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def less_than_4_answers
-    errors.add(:more_than_4_answers, "can't have more than 4 answers") if question.answers.count > 4 
+    errors.add(:more_than_4_answers, "can't have more than 4 answers") if question.answers.count > 3 
   end
 end
